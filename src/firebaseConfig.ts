4f3f5098970a78dfd9fa0aa3768967dd539a8d9d
Shivam Prasad <321shivamivaan@gmail.com>
2025-10-12 02:01:43 +0530

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
+import.meta.env.VITE_FIREBASE_API_KEY
 
 // 🔐 use Vercel environment variables (Settings → Environment Variables)
 const firebaseConfig = {
